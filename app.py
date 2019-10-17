eb6ff9a831e464d219b25f087386937d1c519768
Abdul Hadi Mirzada <mirz-05@hotmail.com>
2019-10-17 19:29:14 +0100

    Website uploaded

--- app.py
+++ app.py
@@ -1,44 +1,13 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
 
 from flask import Flask, render_template, request
-# from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
 
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 app.config.from_object('config')
-#db = SQLAlchemy(app)
-
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
-
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
-#----------------------------------------------------------------------------#
-# Controllers.
-#----------------------------------------------------------------------------#
 
 
 @app.route('/')
@@ -62,14 +31,13 @@ def register():
     form = RegisterForm(request.form)
     return render_template('forms/register.html', form=form)
 
+@app.route('/cancer')
+def cancer():
+    return render_template('pages/cancer.html')
 
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-# Error handlers.
-
+@app.route('/diabetes')
+def diabetes():
+    return render_template('pages/diabetes.html')
 
 @app.errorhandler(500)
 def internal_error(error):
@@ -91,17 +59,6 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
 
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
 
-# Default port:
 if __name__ == '__main__':
     app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
--- forms.py
+++ forms.py
@@ -2,9 +2,6 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Length
 
-# Set your classes here.
-
-
 class RegisterForm(Form):
     name = TextField(
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
